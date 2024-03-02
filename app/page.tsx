fbbc25ce5e8a8a1840e6187f7dfbae7ea68e98d6
LeoMcBills <leokinyera9@gmail.com>
2024-03-02 17:58:45 +0300

    full package

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { text } from "stream/consumers";
+
+export default function () {
+  return (
+    <>
+      <h1 className="bg-zinc-900 text-wrap text-white md:text-orange sm:text-green lg:text-yellow">Leo is the greatest programmer existing on the planet today </h1>
+      <h2>Leo is the next greatest being to ever exist</h2>
+    </>
+  )
+}
